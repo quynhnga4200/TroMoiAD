ddc91aafd99aebaa5ccfe821053a80768b80dd0d
= <20211TT0219@mail.tdc.edu.vn>
2022-05-20 00:04:42 +0700

    hoan thanh giao dien login vs motel_detail

--- app/src/main/java/com/example/myapplication/MotelDetail.java
+++ app/src/main/java/com/example/myapplication/MotelDetail.java
@@ -1,14 +1,14 @@
-package tdc.edu.tromoiproject;
+package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MotelDetail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_motel_detail);
     }
 }
