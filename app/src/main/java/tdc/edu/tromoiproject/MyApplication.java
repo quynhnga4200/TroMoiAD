3b471b8e95871334830b183ff962b45eb80fd807
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-23 23:13:22 +0700

    thông báo từ firebase

--- app/src/main/java/tdc/edu/tromoiproject/MyApplication.java
+++ app/src/main/java/tdc/edu/tromoiproject/MyApplication.java
@@ -1,2 +1,28 @@
-package tdc.edu.tromoiproject;public class MyApplication {
+package tdc.edu.tromoiproject;
+
+import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+
+public class MyApplication extends Application {
+   public static final String CHANNER_ID = "push" ;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        createChanelNotifytion();
+    }
+
+    private void createChanelNotifytion() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel Channel =new NotificationChannel(CHANNER_ID,"PUSH NOTIFYCATION",
+                    NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(Channel);
+
+        }
+
+    }
 }
--- app/src/main/java/tdc/edu/tromoiproject/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/tdc/edu/tromoiproject/fcm/MyFirebaseMessagingService.java
@@ -1,2 +1,47 @@
-package tdc.edu.tromoiproject.fcm;public class MyFirebaseMessagingService {
+package tdc.edu.tromoiproject.fcm;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import tdc.edu.tromoiproject.MyApplication;
+import tdc.edu.tromoiproject.R;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    @Override
+    public void onMessageReceived(@NonNull RemoteMessage message) {
+        super.onMessageReceived(message);
+        RemoteMessage.Notification notification = message.getNotification();
+        if (notification == null){
+            return;
+        }
+        String strTitle = notification.getTitle();
+        String strMessage = notification.getBody();
+        sendNotification(strTitle,strMessage);
+    }
+
+    private void sendNotification(String strTitle, String strMessage) {
+        Intent intent = new Intent(this,MyApplication.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new
+                NotificationCompat.Builder(this, MyApplication.CHANNER_ID)
+                .setContentTitle(strTitle)
+                .setContentText(strMessage)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent);
+      Notification notification = builder.build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null){
+            notificationManager.notify(1,notification);
+        };
+    }
 }
