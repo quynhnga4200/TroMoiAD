e0c1e8f8a7cc62fe6f9ab908ae244efadc133c25
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-24 14:33:26 +0700

    cập nhật thêm dữ liệu

--- app/src/main/java/tdc/edu/tromoiproject/LoginActivity.java
+++ app/src/main/java/tdc/edu/tromoiproject/LoginActivity.java
@@ -2,13 +2,57 @@ package tdc.edu.tromoiproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
+    private EditText textMail;
+    private  EditText textPass;
+    private Button DangNhap;
+    private Button QuenMK;
+    private TextView DangKi;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        initUI();
+        QuenMK.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToVerifyActivity();
+            }
+        });
+        DangKi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToregisterActivity();
+            }
+        });
+
+    }
+    private void goToVerifyActivity() {
+        Intent intent = new Intent(this, VeryPhoneNumberActivity.class);
+        startActivity(intent);
+    }
+    private void goToregisterActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+    public void getDataIntent(){
+        String strphoneNumber = getIntent().getStringExtra("phone_number");
+        EditText text =findViewById(R.id.TextMail);
+        text.setText(strphoneNumber);
+
+    }
+    private  void initUI(){
+        textMail = findViewById(R.id.TextMail);
+        textPass = findViewById(R.id.editpassword);
+        DangNhap = findViewById(R.id.btnlogin);
+        DangKi = findViewById(R.id.edtDangKi);
+        QuenMK = findViewById(R.id.textQuenPass);
     }
 }
--- app/src/main/java/tdc/edu/tromoiproject/VeryPhoneNumberActivity.java
+++ app/src/main/java/tdc/edu/tromoiproject/VeryPhoneNumberActivity.java
@@ -3,14 +3,21 @@ package tdc.edu.tromoiproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthOptions;
 import com.google.firebase.auth.PhoneAuthProvider;
@@ -18,6 +25,7 @@ import com.google.firebase.auth.PhoneAuthProvider;
 import java.util.concurrent.TimeUnit;
 
 public class VeryPhoneNumberActivity extends AppCompatActivity {
+    private  final  String TAG = VeryPhoneNumberActivity.class.getName();
     private EditText editphone;
     private Button buttonvery;
     private FirebaseAuth mAuth;
@@ -27,6 +35,7 @@ public class VeryPhoneNumberActivity extends AppCompatActivity {
         setContentView(R.layout.activity_very_phone_number);
         SetTitleToolsBar();
         initUI();
+        mAuth = FirebaseAuth.getInstance();
         buttonvery.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -46,23 +55,64 @@ public class VeryPhoneNumberActivity extends AppCompatActivity {
                         .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
                             @Override
                             public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
-
+                                signInWithPhoneAuthCredential(phoneAuthCredential);
                             }
 
                             @Override
                             public void onVerificationFailed(@NonNull FirebaseException e) {
+                                Toast.makeText(VeryPhoneNumberActivity.this, "OVerification Failed", Toast.LENGTH_SHORT).show();
 
                             }
                             @Override
                             public void onCodeSent(@NonNull String verificationId,
                                                    @NonNull PhoneAuthProvider.ForceResendingToken token) {
                                 super.onCodeSent(verificationId,token);
+                                goToEnterOTP(strNumber,verificationId);
+
                             }
                         })          // OnVerificationStateChangedCallbacks
                         .build();
         PhoneAuthProvider.verifyPhoneNumber(options);
     }
 
+
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.e(TAG, "signInWithCredential:success");
+
+                            FirebaseUser user = task.getResult().getUser();
+                            // Update UI
+                            goToMainActivity(user.getPhoneNumber());
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(VeryPhoneNumberActivity.this,
+                                         "The verification code entered was invalid", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void goToMainActivity(String phoneNumber) {
+        Intent intent = new Intent(this,LoginActivity.class);
+        intent.putExtra("phone number", phoneNumber);
+        startActivity(intent);
+    }
+    private void goToEnterOTP(String strNumber, String verificationId) {
+        Intent intent = new Intent(this,OtpNumberActivity.class);
+        intent.putExtra("phone_number", strNumber);
+        intent.putExtra("verify_cation_ID", verificationId);
+        startActivity(intent);
+    }
     private  void initUI(){
         editphone = findViewById(R.id.forgetphone);
         buttonvery = findViewById(R.id.veryphonenumber);
