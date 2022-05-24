dc8a4671a364409cceb2e76477036fc9af9bb6c4
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-24 21:48:01 +0700

    paarrt 6

---
+++ app/src/main/java/tdc/edu/tromoiproject/SplatsActivity.java
@@ -0,0 +1,14 @@
+package tdc.edu.tromoiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplatsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splats);
+    }
+}
