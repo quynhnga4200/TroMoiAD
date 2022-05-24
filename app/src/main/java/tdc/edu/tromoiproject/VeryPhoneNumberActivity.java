f227a4d837b3a9af506f9daaed168627a06e4644
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-24 11:55:49 +0700

    thêm thư viện

--- app/src/main/java/tdc/edu/tromoiproject/VeryPhoneNumberActivity.java
+++ app/src/main/java/tdc/edu/tromoiproject/VeryPhoneNumberActivity.java
@@ -1,14 +1,77 @@
 package tdc.edu.tromoiproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
 
 public class VeryPhoneNumberActivity extends AppCompatActivity {
-
+    private EditText editphone;
+    private Button buttonvery;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_very_phone_number);
+        SetTitleToolsBar();
+        initUI();
+        buttonvery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String strNumber = editphone.getText().toString().trim();
+                onClickVeryfy(strNumber);
+            }
+
+
+        });
+    }
+    private void onClickVeryfy(String strNumber) {
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(strNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+
+                            }
+                            @Override
+                            public void onCodeSent(@NonNull String verificationId,
+                                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                                super.onCodeSent(verificationId,token);
+                            }
+                        })          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
+
+    private  void initUI(){
+        editphone = findViewById(R.id.forgetphone);
+        buttonvery = findViewById(R.id.veryphonenumber);
+
+    }
+
+    private  void  SetTitleToolsBar(){
+        if (getSupportActionBar() != null){
+            getSupportActionBar().setTitle("Verify Phone");
+        }
     }
 }
