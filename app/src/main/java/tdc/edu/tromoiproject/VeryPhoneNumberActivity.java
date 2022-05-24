a5fd9f64e3e2d1cd9b38a444df29b2d4b6ab340e
trannhatthanh <20211tt1281@mail.tdc.edu.vn>
2022-05-24 11:30:20 +0700

    làm lại

---
+++ app/src/main/java/tdc/edu/tromoiproject/VeryPhoneNumberActivity.java
@@ -0,0 +1,14 @@
+package tdc.edu.tromoiproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VeryPhoneNumberActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_very_phone_number);
+    }
+}
